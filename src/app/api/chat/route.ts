f0f9ec40027e0c93763851db700653190f84210e
mohamed el merjani <mohamedelmerjani@gmail.com>
2024-07-08 01:38:46 +0100

    adding some details

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,11 @@
+import {openai} from '@ai-sdk/openai';
+import {streamText} from 'ai';
+
+export async function POST(req:Request){
+  const{messages}=await  req.json();
+  const result= await streamText({
+    model:openai('gpt-3.5-turbo-0125'),
+    messages,
+  });
+  return new StreamingTextResponse(result.toAIStream());
+}
